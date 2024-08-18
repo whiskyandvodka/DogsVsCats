0ca81b0db42603cc114bcd8d99cc3ef7c97e60e7
Mr.kuang <3077363431@qq.com>
2024-08-18 23:18:18 +0800

    train编写中

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+import warnings
+import torch as t
+
+
 class DefaultConfig(object):
-    # env = 'default'       # Visdom环境
+    env = 'default'       # Visdom环境
     model = 'SqueezeNet'    # 使用的模型，名字必须与models/__init__.py中的名字一致
     train_data_root = ''
     test_data_root = ''
@@ -17,5 +21,26 @@ class DefaultConfig(object):
     lr = 0.1                # 初始化学习率
     lr_decay = 0.95         # 学习率衰减，lr = lr * lr_decay
     weight_decay = 1e-4
+
+    def parse(self, kwargs):
+        """
+        根据字典kwargs更新配置参数
+        :param kwargs:
+        :return:
+        """
+        for k, v in kwargs.items():
+            if not hasattr(self, k):
+                warnings.warn("Warning: opt has not attribute %s" % k)
+                setattr(self, k, v)
+
+        # 打印配置信息
+        print('user config:')
+        for k, v in self.__class__.__dict__.items():
+            if not k.startswith('__'):
+                print(k, getattr(self, k))
+
+
+opt = DefaultConfig()
+
 
 
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os.path
+
 import models
+import torch as t
+from config import opt
 from config import DefaultConfig
+from data.dataset import DogCat
+from torch.utils.data import DataLoader
+from torchnet import meter
+from utils.visualize import Visualizer
+from tqdm import tqdm
 
 
 def train(**kwargs):
@@ -10,4 +19,64 @@ def train(**kwargs):
     """
     # 根据命令行参数更新配置
     opt.parse(kwargs)
+    vis = Visualizer(opt.env)
+
+    # step1:定义网络模型
+    model = getattr(models, opt.model)
+    if opt.load_model_path:
+        model.load(opt.load_model_path)
+    if opt.use_gpu:
+        model.cuda()
+
+    # step2:数据预处理和加载
+    train_data = DogCat(opt.train_data_root, mode="train")
+    val_data = DogCat(opt.train_data_root, mode="val")
+    train_dataloader = DataLoader(train_data, opt.batch_size,
+                                  shuffle=True, num_workers=opt.num_workers)
+    val_dataloader = DataLoader(val_data, opt.batch_size,
+                                shuffle=False, num_workers=opt.num_workers)
+
+    # step3:定义损失函数和优化器
+    criterion = t.nn.CrossEntropyLoss()
+    lr = opt.lr
+    optimizer = t.optim.Adam(model.parameters(), lr=lr, weight_decay=opt.weight_decay)
+
+    # step4:计算指标，如平滑处理后之后的损失，还有混淆矩阵
+    loss_meter = meter.AverageValueMeter()
+    confusion_matrix = meter.ConfusionMeter(2)
+    previous_loss = 1e100
+
+    # 训练
+    for epoch in range(opt.max_epoch):
+        loss_meter.reset()
+        confusion_matrix.reset()
+
+        for ii, (data, label) in enumerate(train_dataloader):
+
+            # 训练模型参数
+            input = data.to(opt.device)
+            target = label.to(opt.device)
+
+            optimizer.zero_grad()
+            score = model(input)
+            loss = criterion(score, target)
+            loss.backward()
+            optimizer.step()
+
+            # 更新统计指标以及可视化
+            loss_meter.add(loss.item())
+            confusion_matrix.add(score.detach(), target.detach())
+
+            if ii % opt.print_freq == opt.print_freq - 1:
+                vis.plot('loss', loss_meter.value()[0])
+
+                # 如果需要，则可以进入调试模式
+                if os.path.exists(opt.debug_file):
+                    import ipdb
+                    ipdb.set_trace()
+
+        model.save()
+
+        # 计算验证集上的指标以及可视化
+
 
